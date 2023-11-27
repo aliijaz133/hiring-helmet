e9a972765f646421d1b4b974192d12a7341fe12e
aliijaz133 <aliejaz0072@gmail.com>
2023-11-28 00:48:10 +0500

    Header File Updating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HiringHelmetComponent } from './hiring-helmet/hiring-helmet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'hiring-helmet',
+    pathMatch: 'full',
+  },
+  {
+    path: 'hiring-helmet',
+    component: HiringHelmetComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'hiring-helmet',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  logo = '.././../assets/image/asd 1.png';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
