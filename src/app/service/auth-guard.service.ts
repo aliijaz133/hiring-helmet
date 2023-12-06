4dc721964bf8e1cedd01024079e493c8996723c6
aliijaz133 <aliejaz0072@gmail.com>
2023-12-06 23:18:25 +0500

    Add Auth Service and Auth Guard

--- src/app/service/auth-guard.service.ts
+++ src/app/service/auth-guard.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthServiceService } from './auth-service.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
+  constructor(
+    private authService: AuthServiceService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  canActivate(): boolean {
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['/signin']);
+      return false;
+    }
+  }
+
+  getUserName() {
+    setTimeout(() => {
+      console.log('Git');
+    });
+  }
 }
--- src/app/service/auth-service.service.ts
+++ src/app/service/auth-service.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthServiceService {
+  public readonly USER_STORAGE_KEY = 'userId';
+  public readonly USERNAME_STORAGE_KEY = 'userName';
 
-  constructor() { }
+  private baseUrl = 'http://localhost:3000/api';
+
+  signIn = ['/signin'];
+
+  constructor(private router: Router, private http: HttpClient) {}
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem(this.USER_STORAGE_KEY);
+  }
+
+  login(userId: string, userName: string): void {
+    localStorage.setItem(this.USER_STORAGE_KEY, userId);
+    localStorage.setItem(this.USERNAME_STORAGE_KEY, userName);
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.USER_STORAGE_KEY);
+    localStorage.removeItem(this.USERNAME_STORAGE_KEY);
+    this.router.navigate(this.signIn);
+  }
+
+  getLoggedInUserName() {
+    return localStorage.getItem(this.USERNAME_STORAGE_KEY);
+  }
 }
