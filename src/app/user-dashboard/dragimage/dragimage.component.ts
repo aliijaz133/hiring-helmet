73415e03223c8e8c269f08fe958e6d36dd9a1708
aliijaz133 <aliejaz0072@gmail.com>
2023-12-07 18:59:54 +0500

    image preview function

--- src/app/user-dashboard/dragimage/dragimage.component.ts
+++ src/app/user-dashboard/dragimage/dragimage.component.ts
@@ -13,7 +13,9 @@ import {
 })
 export class DragimageComponent implements OnInit {
   deliverOrder: FormGroup;
-  imageUrl: string | ArrayBuffer | null = null;
+  imageUrl = '../../../assets/image/Vector.png';
+
+  fileInput: any;
 
   constructor(private fb: FormBuilder) {
     this.deliverOrder = this.fb.group({
@@ -22,24 +24,20 @@ export class DragimageComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.fileInput = document.getElementById('file');
+  }
 
   onFileChange(event: any) {
-    const file = event.target.files[0];
-    console.log('Selected File:', file);
+    if (event.target.files) {
+      const reader = new FileReader();
 
-    this.deliverOrder.patchValue({
-      deliveryImage: file,
-    });
+      reader.readAsDataURL(event.target.files[0]);
 
-    // Display the uploaded image
-    const reader = new FileReader();
-    reader.onload = (e: any) => {
-      console.log('Image Loaded:', e);
-      this.imageUrl = e.target.result;
-    };
-
-    reader.readAsDataURL(file);
+      reader.onload = (e: any) => {
+        this.imageUrl = e.target.result;
+      };
+    }
   }
 
   submitOrder() {
@@ -49,14 +47,11 @@ export class DragimageComponent implements OnInit {
 
       console.log(
         'Image Size:',
-        imageSize ? imageSize.sizeOf : 'No image selected'
+        imageSize ? imageSize.size : 'No image selected'
       );
       console.log('Details:', details);
-
-      // Add your submit logic here
     }
 
     this.deliverOrder.reset();
-    this.imageUrl = null; // Reset the image URL after form submission
   }
 }
